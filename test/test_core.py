3ec8af30eb977a782f0ac2b2e774c9aa4dab56c5
MartinaMoglioni <martina.moglioni@libero.it>
2020-10-19 17:04:20 +0200

    Folder test with all the devices

---
+++ test/test_core.py
@@ -0,0 +1,14 @@
+''' Prima tutti gli import della SL di Python, poi tutti i from '''
+import unittest
+
+from smartsquare.core import square
+
+class TestCore(unittest.TestCase):
+    ''' Definiamo tutti i metodi di questa classe '''
+    def test_float(self):
+        ''' test with floats '''
+        self.assertAlmostEqual(square(2.), 4.)
+
+
+if __name__=='__main__':
+    unittest.main()
